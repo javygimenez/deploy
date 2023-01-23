a72548e0bfa72976498acb4d308cad614b2bbf6a
javygimenez <soy_entrerriano@hotmail.com>
2023-01-22 23:25:27 -0300

    agrego ruta home

--- src/server.js
+++ src/server.js
@@ -7,6 +7,9 @@ const app = express();
 app.get('/', (req, res)=>{
     res.send('<h1>Bienvenido a mi servidor!</h1>')
 });
+app.get('/home', (req, res)=>{
+    res.send('<h1>Este es un servidor en Railway</h1>')
+});
 
 const PORT = process.env.PORT || 8080;
 
